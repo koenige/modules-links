c1d8df49aadce543766143012933e8af7d955bd4
Gustaf Mossakowski <gustaf@koenige.org>
2021-11-17 16:31:26 +0000

    links/events: use placeholder for date format for event instead of hardcoding German format
    git-svn-id: file:///Repos/modules/links/trunk@47 588cf18a-5e91-4ba5-a510-67a2037605c1

--- zzbrick_tables/links-events.php
+++ zzbrick_tables/links-events.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/links
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2017-2018 Gustaf Mossakowski
+ * @copyright Copyright © 2017-2018, 2021 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -31,22 +31,22 @@ $zz['fields'][2]['display_field'] = 'link_url';
 $zz['fields'][3]['field_name'] = 'event_id';
 $zz['fields'][3]['type'] = 'select';
 $zz['fields'][3]['type'] = 'select';
-$zz['fields'][3]['sql'] = 'SELECT event_id
-	, CONCAT(/*_PREFIX_*/events.event, " (", DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%d.%m.%Y")
-		, IFNULL(CONCAT(", ", place), ""), ")") AS event 
+$zz['fields'][3]['sql'] = sprintf('SELECT event_id
+	, CONCAT(/*_PREFIX_*/events.event, " (", DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%s")
+		, ")") AS event 
 	FROM /*_PREFIX_*/events
 	WHERE ISNULL(main_event_id)
-	ORDER BY date_begin DESC';
+	ORDER BY date_begin DESC', wrap_placeholder('mysql_date_format'));
 $zz['fields'][3]['display_field'] = 'event';
-$zz['fields'][3]['search'] = 'CONCAT(/*_PREFIX_*/events.event, " (", 
-	DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%d.%m.%Y"), IFNULL(CONCAT(", ", place), ""), ")")';
+$zz['fields'][3]['search'] = sprintf('CONCAT(/*_PREFIX_*/events.event, " (", 
+	DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%s"), ")")', wrap_placeholder('mysql_date_format'));
 
-$zz['sql'] = 'SELECT /*_PREFIX_*/links_events.*
+$zz['sql'] = sprintf('SELECT /*_PREFIX_*/links_events.*
 		, /*_PREFIX_*/links.link_url
-		, CONCAT(/*_PREFIX_*/events.event, " (", DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%d.%m.%Y")
-			, IFNULL(CONCAT(", ", place), ""), ")") AS event 
+		, CONCAT(/*_PREFIX_*/events.event, " (", DATE_FORMAT(/*_PREFIX_*/events.date_begin, "%s")
+			, ")") AS event 
 	FROM /*_PREFIX_*/links_events
 	LEFT JOIN /*_PREFIX_*/links USING (link_id)
 	LEFT JOIN /*_PREFIX_*/events USING (event_id)
-';
+', wrap_placeholder('mysql_date_format'));
 $zz['sqlorder'] = ' ORDER BY /*_PREFIX_*/links.sequence, IFNULL(/*_PREFIX_*/events.date_begin, /*_PREFIX_*/events.date_end)';
